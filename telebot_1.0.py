de47c2227c23d5f313dbaa5d898929ad242c19e0
joponaderun <109222134+joponaderun@users.noreply.github.com>
2022-07-15 08:46:06 +0300

    Update telebot_1.0.py
    deleted token

--- telebot_1.0.py
+++ telebot_1.0.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('5400061015:AAFONAnfRMWtE0sGvcs1HAKopTbJFuBE0tw')
+bot = telebot.TeleBot('')
 
 @bot.message_handler(commands=['help'])
 def website(message):
