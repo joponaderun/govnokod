8ca4b160d1960a467f8752064f6e108f3235d369
joponaderun <doyoulikepesiki@yandex.ru>
2022-07-18 17:49:37 +0300

    Update telebot_1.0.py
    set to offline mod

--- telebot_1.0.py
+++ telebot_1.0.py
@@ -13,7 +13,7 @@ with open("token.txt") as f:
 # os.environ[TOKEN]
 #bot = telebot.TeleBot(TOKEN)
 bot = telebot.TeleBot('5400061015:AAFONAnfRMWtE0sGvcs1HAKopTbJFuBE0tw')
-
+'''
 @bot.message_handler(commands=['help'])
 def website(message):
     markup = types.ReplyKeyboardMarkup()
@@ -21,6 +21,15 @@ def website(message):
     Btn_2 = types.KeyboardButton('кнопка 2')
     markup.add(Btn_1, Btn_2)
     bot.send_message(message.chat.id, 'test', reply_markup=markup)
+'''
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Старт":
+        bot.send_message(message.from_user.id, "Окей, погнали")
+    elif message.text == "Стоп":
+        bot.send_message(message.from_user.id, "Окей, закругляемся")
+    else:
+        bot.send_message(message.from_user.id, "не пиши херню!")
 
 
 bot.infinity_polling()
